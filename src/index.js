9ca96b7b3ca8b1f9c06937192473ff9a608eab6b
Svetlna <svetlana@dev.com>
2023-10-08 16:02:46 +0300

    created brain-calc.js refactor index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import {startGame} from "./startGame.js";
+
+export const even = () => {
+    startGame('even');
+}
+
+export const calc = () => {
+    startGame('calc');
+}
+
+export const gcd = () => {
+    startGame('gcd');
+};
+
+export const progression = () => {
+    startGame('progression');
+}
+
+export const prime = () => {
+    startGame('prime');
+}
+
